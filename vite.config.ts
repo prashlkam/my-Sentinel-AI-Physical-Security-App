aa6dd97b26911d5eac458969cba2836ed314278c
Prashanth Kamath <prashanth_kamath_2001@hotmail.com>
2026-04-11 18:09:00 +0530

    Reorder allowedHosts in vite.config.ts
    Reorder allowedHosts in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: port,
         host: '0.0.0.0',
-        allowedHosts: ['localhost', 'mywebapp03-d9fffxh5avayg9bk.southindia-01.azurewebsites.net'],
+        allowedHosts: ['mywebapp03-d9fffxh5avayg9bk.southindia-01.azurewebsites.net', 'localhost'],
         strictPort: false,
       },
       preview: {
